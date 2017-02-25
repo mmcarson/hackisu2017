46a5812a0ab34b1b48cafdd49d344b109accf5bb
bugsquasher <camelot70@peoplepc.com>
2017-02-25 14:04:06 -0600

    Changes for database connection

---
+++ Veloxopus/app/src/main/java/com/example/sabishop/veloxopus/MySQLDatabase.java
@@ -0,0 +1,17 @@
+package com.example.sabishop.veloxopus;
+
+import java.sql.*;
+
+/**
+ * Created by Marc on 2/25/2017.
+ */
+
+public class MySQLDatabase {
+
+    protected Connection connection;
+    protected Statement stmt;
+
+    
+
+
+}
