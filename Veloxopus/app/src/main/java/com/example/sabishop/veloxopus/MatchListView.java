d81dcc57b53b0e6936c76acb22770a19a0265d4e
Marissa Carson <mmcarson@coe.edu>
2017-02-25 15:25:53 -0600

    Made MatchListView (isn't done yet or used yet)
    Looks like ProfileListView but light purple and with some different
    buttons

---
+++ Veloxopus/app/src/main/java/com/example/sabishop/veloxopus/MatchListView.java
@@ -0,0 +1,94 @@
+package com.example.sabishop.veloxopus;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by marissacarson on 2/24/17.
+ */
+
+public class MatchListView extends LinearLayout {
+    protected Context context;
+
+    int screenWidth, screenHeight;
+    static int PADDING_HORIZONTAL = 20, PADDING_VERTICAL = 0, PADDING_INIT = 40;
+    Profile profile;
+    Activity parent;
+
+    public MatchListView(Context context, final Profile profile, final Activity parent) {
+        super(context);
+        setOrientation(HORIZONTAL);
+        this.context = context;
+        this.profile = profile;
+
+        //get screen size
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+        this.setBackground(new ColorDrawable(getResources().getColor(R.color.colorMediumPurple)));
+
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+        setGravity(Gravity.CENTER_VERTICAL);
+
+        setPadding(screenWidth / 7, screenWidth / 12, screenWidth / 12, screenWidth / 12);
+        lp.height = screenHeight/6;
+        lp.width = screenWidth;
+        TextView nameView = new TextView(context);
+        nameView.setTextColor(Color.WHITE);
+        nameView.setTypeface(Typeface.create("HELVETICA", Typeface.BOLD));
+        nameView.setText(profile.type + ": " + profile.name);
+        this.addView(nameView);
+        nameView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        ImageView chat = new ImageView(context);
+        chat.setImageDrawable(getResources().getDrawable(R.drawable.icons_white_speech));
+        chat.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+                Toast.makeText(getContext(), "This will go to chat", Toast.LENGTH_LONG).show();
+            }
+        });
+        chat.setMaxHeight(screenHeight/6);
+        chat.setMaxWidth(screenHeight/6);
+        chat.setPadding(PADDING_HORIZONTAL, PADDING_VERTICAL, PADDING_HORIZONTAL, PADDING_VERTICAL);
+        addView(chat);
+
+        ImageView close = new ImageView(context);
+        close.setImageDrawable(getResources().getDrawable(R.drawable.icons_white_close));
+        close.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+        close.setMaxHeight(screenHeight/6);
+        close.setMaxWidth(screenHeight/6);
+        close.setPadding(PADDING_HORIZONTAL, PADDING_VERTICAL, PADDING_HORIZONTAL, PADDING_VERTICAL);
+        addView(close);
+    }
+}
